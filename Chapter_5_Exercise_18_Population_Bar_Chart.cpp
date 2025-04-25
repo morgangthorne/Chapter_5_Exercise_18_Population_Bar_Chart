5a4b5ecf0c8026fec8d87ac6a7f4334368c047b8
morgan thorne <morgangthorne@outlook.com>
2025-04-25 15:54:43 -0400

    Original Code

--- Chapter_5_Exercise_18_Population_Bar_Chart.cpp
+++ Chapter_5_Exercise_18_Population_Bar_Chart.cpp
@@ -1,5 +1,13 @@
-// Chapter_5_Exercise_18_Population_Bar_Chart.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/* 
+FileName: Chapter_5_Exercise_18_Population_Bar_Chart.cpp
+Programmer: Morgan Thorne
+Date: April 2025
+Requirements:
+Write a program that produces a bar chart showing the population growth of Prairieville, a small town in the Midwest, 
+at 20-year intervals during the past 100 years. The program should read in the population figures (rounded to the nearest 1,000 people) 
+for 1900, 1920, 1940, 1960, 1980, and 2000 from a file. For each year, it should display the date and a bar consisting of one asterisk 
+for each 1,000 people. The data can be found in the People.txt file.
+*/
 
 #include <iostream>
 #include <string>
